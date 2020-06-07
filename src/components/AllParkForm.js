03a87c686ba7498e0c86a97423b9476f5cf937eb
github username <github email address>
2020-06-07 14:35:32 -0700

    neighbourhood to form

--- src/components/AllParkForm.js
+++ src/components/AllParkForm.js
@@ -1,68 +1,28 @@
 import React, { Component } from 'react'
 
-const FEATURESALL=["Golf",
-"Play Area (ADA Compliant)",
-"Basketball (Full)",
-"Bike Trail",
-"Play Area",
-"Paths",
-"Waterfront",
-"P-Patch Community Garden",
-"Soccer",
-"View",
-"Woods",
-"Historic Landmark",
-"Creek",
-"Garden",
-"Boat Launch (Hand Carry)",
-"Adult Fitness Equipment",
-"Guarded Beach",
-"Restrooms (ADA Compliant)",
-"Wading Pool or Water Feature",
-"Tennis Lights",
-"Picnic Sites",
-"Disc Golf",
-"Restrooms",
-"Weddings and Ceremonies",
-"Green Space",
-"Paths (ADA Compliant)",
-"Baseball/Softball",
-"Fishing",
-"Basketball (Half)",
-"Rental Facility",
-"Hiking Trails",
-"Pesticide Free",
-"Decorative Fountain",
-"Tennis Court (Outdoor)",
-"Horseshoe Pits",
-"Dog Off Leash Area",
-"Skatepark",
-"Football",
-"Marination Ma Kai",
-"Lawn Bowling",
-"Tennis Backboard (Outdoor)",
-"Scuba Diving",
-"Community Building",
-"Environmental Learning Center",
-"NO Beach Access",
-"Fire Pit",
-"Pool (Outdoor)",
-"Lacrosse",
-"Boat Moorage",
-"Picnic Sites (ADA Compliant)",
-"Skatespot",
-"Model Boat Pond",
-"Community Center",
-"Cricket",
-"Ultimate",
-"Track",
-"Rugby",
-"Boat Launch (Motorized)",
-"Bike Polo",
-"Pool (Indoor)",
-"T-Ball",
-"Pickleball Court",
+const FEATURESALL=["Golf", "Play Area (ADA Compliant)", "Basketball (Full)", "Bike Trail", "Play Area",
+"Paths", "Waterfront", "P-Patch Community Garden",
+"Soccer", "View", "Woods", "Historic Landmark", "Creek", "Garden",
+"Boat Launch (Hand Carry)", "Adult Fitness Equipment", "Guarded Beach", "Restrooms (ADA Compliant)",
+"Wading Pool or Water Feature", "Tennis Lights", "Picnic Sites", "Disc Golf", "Restrooms",
+"Weddings and Ceremonies", "Green Space", "Paths (ADA Compliant)",
+"Baseball/Softball", "Fishing", "Basketball (Half)", "Rental Facility", "Hiking Trails",
+"Pesticide Free", "Decorative Fountain","Tennis Court (Outdoor)", "Horseshoe Pits",
+"Dog Off Leash Area", "Skatepark", "Football", "Marination Ma Kai",
+"Lawn Bowling", "Tennis Backboard (Outdoor)", "Scuba Diving", "Community Building",
+"Environmental Learning Center", "NO Beach Access", "Fire Pit", "Pool (Outdoor)", "Lacrosse",
+"Boat Moorage", "Picnic Sites (ADA Compliant)", "Skatespot",
+"Model Boat Pond", "Community Center", "Cricket", "Ultimate",
+"Track", "Rugby", "Boat Launch (Motorized)", "Bike Polo", "Pool (Indoor)", "T-Ball", "Pickleball Court",
 "Flag Football"]
+
+const NEIGHBORHOOD= ["University District", "Rainier Beach", "Central Business District", "South Lake Union", "West Edge", 
+"Eastlake", "Admiral", "Beacon Hill", "Ballard", "Leschi", "Mount Baker", "Columbia City", "Broadview", 
+"View Ridge", "Morgan Junction", "Phinney Ridge", "Cedar Park", "South Park", "Portage Bay", "Queen Anne", 
+"Delridge", "Licton Springs", "Wedgwood", "Madrona", "Denny-Blaine", "Central District", "Montlake", 
+"Belltown", "Seward Park", "Fremont", "Green Lake", "Madison Valley", "Madison Park", "Kenwood", "Westlake", 
+"Lakeridge", "Magnolia", "Sand Point", "Maple Leaf", "Capitol Hill", "Wallingford", "Washington Park", "Haller Lake", 
+"Northgate", "Greenwood"] 
 
 export class AllParkForm extends Component {
     state={
@@ -105,21 +65,18 @@ export class AllParkForm extends Component {
 
                     <div className="form-group">
                         <label className="col-md-4 control-label">Search By Features</label>
+                        <div className="col-md-4">
                              {FEATURESALL.map((feat, index)=> <label key={index} className="checkbox-inline"><input onChange={(e)=>this.handleChangefeatures(e)} type="checkbox" name="checkboxes" value={feat} />{feat}</label> )}
+                             </div>
                     </div>
 
                     <div className="form-group">
                         <label className="col-md-4 control-label">Search by Neighborhood</label>
-                        <div className="col-md-4">
-                            <div className="radio">
-                                <label>
-                                    <input type="radio" name="radios" id="radios-0" value="1"/>Option one</label>
+                        <div className="col-md-4 pt-2">
+                            <select>
+                                {NEIGHBORHOOD.map((neigh, index)=> <option key={index} value={neigh}>{neigh}</option> )}
+                            </select>
                             </div>
-                            <div className="radio">
-                                <label>
-                                    <input type="radio" name="radios" id="radios-1" value="2" />Option two</label>
-                            </div>
-                        </div>
                     </div>
 
                     <div className="form-group">
