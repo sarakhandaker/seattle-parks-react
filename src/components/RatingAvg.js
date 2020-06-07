b570facd55642b226de037283b3e998e2e525c2e
github username <github email address>
2020-06-07 12:04:50 -0700

    rating avg done

--- src/components/RatingAvg.js
+++ src/components/RatingAvg.js
@@ -17,9 +17,19 @@ export class RatingAvg extends Component {
   }
 
   render() {
-    let average= Math.round(this.props.ratings.reduce((sum,num)=>sum+num,0)/this.props.ratings.length)
-    if (!this.props.ratings.length){
+    const length= this.props.ratings.length
+    let average= Math.round(this.props.ratings.reduce((sum,num)=>sum+num,0)/length)
+
+    if (!length){
       average=0
+    }
+    else {
+      var counts = {}
+      for (var i = 0; i < length; i++) {
+        let num = this.props.ratings[i]
+        counts[num] = counts[num] ? counts[num] + 1 : 1;
+      }
+
     }
     return (
       <>
@@ -39,12 +49,11 @@ export class RatingAvg extends Component {
           </div>
           <div className="pull-left" style={{ "width": "180px" }}>
             <div className="progress" style={{ "height": "9px", "margin": "8px 0" }}>
-              <div className="progress-bar progress-bar-success" role="progressbar" aria-valuenow="5" aria-valuemin="0" aria-valuemax="5" style={{ "width": "1000%" }}>
-                <span className="sr-only">80% Complete (danger)</span>
+              <div className="progress-bar progress-bar-success" role="progressbar" aria-valuenow="5" aria-valuemin="0" aria-valuemax="5" style= {{ "width": `${ (counts['5']/length)*100}%` }}>
               </div>
             </div>
           </div>
-          <div className="pull-right" style={{ "marginLeft": "10px" }}>1</div>
+    <div className="pull-right" style={{ "marginLeft": "10px" }}>{counts? counts["5"]: "0"}</div>
         </div>
         <div className="pull-left">
           <div className="pull-left" style={{ "width": "35px", "lineHeight": 1 }}>
@@ -52,12 +61,11 @@ export class RatingAvg extends Component {
           </div>
           <div className="pull-left" style={{ "width": "180px" }}>
             <div className="progress" style={{ "height": "9px", "margin": "8px 0" }}>
-              <div className="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="4" aria-valuemin="0" aria-valuemax="5" style={{ "width": "1000%" }}>
-                <span className="sr-only">80% Complete (danger)</span>
+              <div className="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="4" aria-valuemin="0" aria-valuemax="5" style= {{ "width": `${ (counts['4']/length)*100}%` }}>
               </div>
             </div>
           </div>
-          <div className="pull-right" style={{ "marginLeft": "10px" }}>1</div>
+          <div className="pull-right" style={{ "marginLeft": "10px" }}>{counts? counts["4"]: "0"}</div>
         </div>
         <div className="pull-left">
           <div className="pull-left" style={{ "width": "35px", "lineHeight": 1 }}>
@@ -65,12 +73,11 @@ export class RatingAvg extends Component {
           </div>
           <div className="pull-left" style={{ "width": "180px" }}>
             <div className="progress" style={{ "height": "9px", "margin": "8px 0" }}>
-              <div className="progress-bar progress-bar-info" role="progressbar" aria-valuenow="3" aria-valuemin="0" aria-valuemax="5" style={{ "width": "60%" }}>
-                <span className="sr-only">80% Complete (danger)</span>
+              <div className="progress-bar progress-bar-info" role="progressbar" aria-valuenow="3" aria-valuemin="0" aria-valuemax="5" style= {{ "width": `${ (counts['3']/length)*100}%` }}>
               </div>
             </div>
           </div>
-          <div className="pull-right" style={{ "marginLeft": "10px" }}>0</div>
+          <div className="pull-right" style={{ "marginLeft": "10px" }}>{counts? counts["3"]: "0"}</div>
         </div>
         <div className="pull-left">
           <div className="pull-left" style={{ "width": "35px", "lineHeight": 1 }}>
@@ -78,12 +85,11 @@ export class RatingAvg extends Component {
           </div>
           <div className="pull-left" style={{ "width": "180px" }}>
             <div className="progress" style={{ "height": "9px", "margin": "8px 0" }}>
-              <div className="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="2" aria-valuemin="0" aria-valuemax="5" style={{ "width": "40%" }}>
-                <span className="sr-only">80% Complete (danger)</span>
+              <div className="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="2" aria-valuemin="0" aria-valuemax="5" style= {{ "width": `${ (counts['2']/length)*100}%` }}>
               </div>
             </div>
           </div>
-          <div className="pull-right" style={{ "marginLeft": "10px" }}>0</div>
+          <div className="pull-right" style={{ "marginLeft": "10px" }}>{counts? counts["2"]: "0"}</div>
         </div>
         <div className="pull-left">
           <div className="pull-left" style={{ "width": "35px", "lineHeight": 1 }}>
@@ -91,12 +97,11 @@ export class RatingAvg extends Component {
           </div>
           <div className="pull-left" style={{ "width": "180px" }}>
             <div className="progress" style={{ "height": "9px", "margin": "8px 0" }}>
-              <div className="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="1" aria-valuemin="0" aria-valuemax="5" style={{ "width": "20%" }}>
-                <span className="sr-only">80% Complete (danger)</span>
+              <div className="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="1" aria-valuemin="0" aria-valuemax="5" style= {{ "width": `${ (counts['1']/length)*100}%` }}>
               </div>
             </div>
           </div>
-          <div className="pull-right" style={{ "marginLeft": "10px" }}>0</div>
+          <div className="pull-right" style={{ "marginLeft": "10px" }}>{counts? counts["1"]: "0"}</div>
         </div>
       </div>
       </>
