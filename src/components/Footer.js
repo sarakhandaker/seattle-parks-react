3bcc6d6cf12859aa09b8697c3b17383f45710653
github username <github email address>
2020-06-15 15:39:56 -0700

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,10 @@ function Footer() {
         return (
             <footer className="bg-light pb-2">
                 <div className="text-center p-3">
-                    <p className="font-italic text-muted mb-0"><strong>Seattle Parks By: Sara Khandaker </strong>2020</p>
+                    <p className="font-italic text-muted mb-0">
+                        <a href="https://www.linkedin.com/in/sara-khandaker-eit-3400b091/" class="social fa fa-linkedin"></a>
+                    <strong>  Seattle Parks By: Sara Khandaker  </strong>2020
+                    <a href="https://github.com/sarakhandaker" class="social fa fa-github"></a></p>
                 </div>
             </footer>
         )
