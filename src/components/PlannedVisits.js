e1ef4bc67489249ecf8cb45ba3d9211110afd5ca
github username <github email address>
2020-06-16 11:49:02 -0700

    modal pic

--- src/components/PlannedVisits.js
+++ src/components/PlannedVisits.js
@@ -10,7 +10,7 @@ export function PlannedVisits({ visits, onRemove }) {
     const [show, setShow] = useState(true);
     const handleClose = () => setShow(false);
 
-    // visits.push({ date: "2020-06-10T00:00:00.000Z", park: "LICTON SPRINGS PARK", park_id: "209" })
+    visits.push({ date: "2020-06-10T00:00:00.000Z", park: "LICTON SPRINGS PARK", park_id: "209" })
 
     const handleDelete = id => {
         api.parks.deleteVisit(id)
@@ -40,7 +40,10 @@ export function PlannedVisits({ visits, onRemove }) {
                 else {
                     return <Modal style={{opacity:1}} show={show} width="400" height="800" onHide={handleClose}>
                         <div className="modal-div">
-                        <Modal.Body> {`How was your planned visit to ${usefulFunctions.title(visit.park)} on ${newDate.toLocaleDateString("en-US")}?`}</Modal.Body>
+                        <Modal.Body> {`How was your visit to ${usefulFunctions.title(visit.park)} on ${newDate.toLocaleDateString("en-US")}?`}</Modal.Body>
+                        <hr/>
+                        <div className="row-md-auto p-3"><img className="bench" src="https://cdn.pixabay.com/photo/2018/09/21/22/46/silhouette-3694193_960_720.png" alt="Bench" style={{"height": "200px"}}/> </div>
+                        <hr/>
                         <Button variant="light" onClick={() => handleLink(visit.id)}><Link to={`/parks/${visit.park_id}`}> Leave a Review </Link> </Button>
                         <Button variant="light" onClick={() => handleLink(visit.id)}><Link to='/home'> Back to Homepage </Link></Button>
                         </div>
